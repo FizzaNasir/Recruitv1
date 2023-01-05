be5faca4420d51aee0feb8508bd13a38e0d69214
Muhammad Talha Masood <talha8665i@gmail.com>
2023-01-05 09:52:06 +0500

    Profile page initiated

---
+++ frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import styles from './Profile.module.css'
+
+const Profile = () => {
+  return <h1>Profile Page</h1>
+}
+
+export default Profile
