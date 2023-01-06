e151d12d7e197ca0ec932f9ca40244fc83a1b063
Fizza <fizzanasir976@gmail.com>
2023-01-06 22:43:23 +0500

    Login completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Signup from './pages/Signup/SignUp'
 import Login from './pages/Login/Login'
+import ResetPwd from './pages/ResetPwd/reset'
+import SetPwd from './pages/ResetPwd/SetPwd'
 function App() {
   return (
     <Routes>
       <Route path='/signup' exact element={<Signup />} />
       <Route path='/login' exact element={<Login />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/resetPwd' exact element={<ResetPwd />} />
+      <Route path='/SetPwd' exact element={<SetPwd />} />
       <Route path='/' exact element={<Navigate replace to='/login' />} />
     </Routes>
   )
