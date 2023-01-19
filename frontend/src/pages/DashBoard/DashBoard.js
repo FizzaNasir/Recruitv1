7a4e043314400a8bca726011e46c9d479602c737
Talha Tariq <talhacoder01@gmail.com>
2023-01-20 00:22:56 +0500

    Job Backend Started

--- frontend/src/pages/DashBoard/DashBoard.js
+++ frontend/src/pages/DashBoard/DashBoard.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 const DashBoard = () => {
-  return <div>RegistrationPage</div>
+  return (
+    <>
+      <Header />
+
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome on Board</h1>
+      </div>
+    </>
+  )
 }
 
 export default DashBoard
