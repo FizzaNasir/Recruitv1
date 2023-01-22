814f05ef8407ee9ffb50faf55533ba928783f82e
Fizza <fizzanasir976@gmail.com>
2023-01-22 15:35:03 +0500

    added missed phone input

--- frontend/src/pages/RegisterCompany/data.js
+++ frontend/src/pages/RegisterCompany/data.js
@@ -1,44 +1,29 @@
+
  const data=[
     {
         Type:'text',
-        // Placeholder:'Company Name',
         Title:'Company Name', 
         Name:'c_name',
         ID: "name"
-        // handleChangestate:{handleChange}
-        // Value={data.password}
     },
     {
         Type:'text',
         Title:'Location', 
-        // Placeholder:'Location',
         Name:'loc',
         ID: "loc"
-        // handleChangestate:{handleChange}
-        // Value={data.password}
     },
     {
         Type:'email',
         Title:'Email',
-        // Placeholder:'Email',
         Name:'email',
         ID:'email'
-        // handleChangestate:{handleChange}
-        // Value={data.password}
-    }
-    // {
-    //     Type:'text',
-    //     Placeholder:'URLs',
-    //     Name:'text',
-    //     // handleChangestate:{handleChange}
-    //     // Value={data.password}
-    // },
-    // {
-    //     Type:'text',
-    //     Placeholder:'URLs',
-    //     Name:'text',
-    //     // handleChangestate:{handleChange}
-    //     // Value={data.password}
-    // }
+    },
+    {
+        Type:'number',
+        Title:'Phone',
+        Name:'phone',
+        ID:'phone',
+        MaxLength: '11'
+    },
 ]
 export default data;
