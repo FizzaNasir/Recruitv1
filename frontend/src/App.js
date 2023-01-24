9e88ee4fdc6ba33a33cc42143d4a429ee327f36c
Muhammad Talha Masood <talha8665i@gmail.com>
2023-01-24 12:40:34 +0500

    Registration page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
-import { CssBaseline, Grid, StyledEngineProvider } from '@mui/material'
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { CssBaseline, Grid, StyledEngineProvider } from '@mui/material';
 import {
   Signup,
   Login,
@@ -16,7 +16,8 @@ import {
   CreateTest,
   SetQuestions,
   PostJob,
-} from './exports'
+  Registration,
+} from './exports';
 
 function App() {
   return (
@@ -24,38 +25,39 @@ function App() {
       <CssBaseline />
       <StyledEngineProvider>
         <Routes>
-          <Route path='/signup' exact element={<Signup />} />
-          <Route path='/login' exact element={<Login />} />
-          <Route path='/resetPwd' exact element={<ResetPwd />} />
-          <Route path='/dashboard' exact element={<DashBoard />} />
-          <Route path='/resetPassword/:token' exact element={<SetPwd />} />
-          <Route path='/verifyYourEmail' exact element={<EmailOtp />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/resetPwd" exact element={<ResetPwd />} />
+          <Route path="/dashboard" exact element={<DashBoard />} />
+          <Route path="/resetPassword/:token" exact element={<SetPwd />} />
+          <Route path="/verifyYourEmail" exact element={<EmailOtp />} />
           <Route
-            path='/enterYourPhoneNbr'
+            path="/enterYourPhoneNbr"
             exact
             element={<PhoneNbrVerify state />}
           />
-          <Route path='/verifyYourPhoneNbr' exact element={<PhoneOtp />} />
-          <Route path='/contactUs' exact element={<ContactUs />} />
+          <Route path="/verifyYourPhoneNbr" exact element={<PhoneOtp />} />
+          <Route path="/register" exact element={<Registration />} />
+          <Route path="/contactUs" exact element={<ContactUs />} />
 
-          <Route path='/test/createTest' exact element={<CreateTest />} />
-          <Route path='/test/set-questions' exact element={<SetQuestions />} />
-          <Route path='/postJob' exact element={<PostJob />} />
+          <Route path="/test/createTest" exact element={<CreateTest />} />
+          <Route path="/test/set-questions" exact element={<SetQuestions />} />
+          <Route path="/postJob" exact element={<PostJob />} />
 
           {/* for page after skiping */}
           {/* <Route path='/PhoneNbrVerify' exact element={<PhoneNbrVerify />} /> */}
-          <Route path='/profile' exact element={<Profile />} />
-          <Route path='/' exact element={<Navigate replace to='/login' />} />
-          <Route path='*' exact element={<ErrorPage />} />
+          <Route path="/profile" exact element={<Profile />} />
+          <Route path="/" exact element={<Navigate replace to="/login" />} />
+          <Route path="*" exact element={<ErrorPage />} />
           <Route
-            path='/register_company'
+            path="/register_company"
             exact
             element={<CompanyRegistration state />}
           />
         </Routes>
       </StyledEngineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
