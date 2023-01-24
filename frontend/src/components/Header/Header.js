9246d5ba9acfb851e30a1730d65fe483b9c707e5
Talha Tariq <talhacoder01@gmail.com>
2023-01-24 20:43:27 +0500

    Dummy Page Added

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './style'
-
+import { useNavigate } from 'react-router-dom'
+import { DashBoard } from '../../exports'
 const Header = () => {
   return (
     <header style={styles.header}>
@@ -9,7 +10,7 @@ const Header = () => {
       <nav>
         <ul style={styles.navList}>
           <li style={styles.navItem}>
-            <Link to='/' style={styles.navLink}>
+            <Link to='/dashboard' style={styles.navLink}>
               Home
             </Link>
           </li>
