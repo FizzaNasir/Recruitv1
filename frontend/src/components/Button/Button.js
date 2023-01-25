dd9cb0b2b6111db99cd1b182f763b58cf940e984
Fizza abid <mefizza54@gmail.com>
2023-01-26 02:57:27 +0500

    contactus frontend

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import './Button.css'
-export default function Button({Variant, Name, Type, Id, HandleClick, Title }) {
-
-    return (
-      <button 
-      name={Name} 
-      type={Type} 
-      id={Id} 
-      className={["btn", Variant].join(" ")} 
+import { useState } from "react";
+import "./Button.css";
+export default function Button({
+  Variant,
+  Name,
+  Type,
+  Id,
+  HandleClick,
+  Title,
+}) {
+  return (
+    <button
+      name={Name}
+      type={Type}
+      id={Id}
+      className={["btn", Variant].join(" ")}
       onClick={HandleClick}
-      >
+    >
       {Title}
-      </button>
-    )
-  }
+    </button>
+  );
+}
